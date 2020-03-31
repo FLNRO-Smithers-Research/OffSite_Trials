4039d38af79490b34208ff7f7617da019464ffb3
Jane Wang <janewyx11@gmail.com>
2020-03-30 23:31:55 -0700

    Re structure code

--- R/data_prep.R
+++ R/data_prep.R
@@ -12,64 +12,50 @@ bec <- read_sf("../BEC/BEC_BIOGEOCLIMATIC_POLY/BEC_POLY_polygon.shp")  # from fi
 
 ## joining beczone with rsk shape
 RSK <- read_sf("../RSK.shp")
-bec_rsk <- st_intersection(RSK, st_buffer(bec, 0))
+bec_rsk_lg <- st_intersection(RSK, st_buffer(bec, 0))
 
 ## working example, retaining 5% of the original details
-bec_rsk <- ms_simplify(bec_rsk)
+bec_rsk <- ms_simplify(bec_rsk_lg)
 
 ## write_sf(bec_rsk, "../bec_rsk.shp", driver = "ESRI Shapefile")
 
 ## reference layer
-ref <- read_csv("data/ReferenceGuide2019_Onsite.csv")
+speciesList = c("FD", "LW", "CW", "PY") 
 
-bgc_join <- left_join(bec_rsk[, c("MAP_LABEL", "FEAT_AREA", "ZONE_NAME")], 
-                      ref[, c("BGC", "SS_NoSpace", "Species")], 
-                      by = c("MAP_LABEL" = "BGC"))
+ref <- read_csv("data/ReferenceGuide2019_Onsite.csv") %>% 
+  mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
+  filter(Species %in% speciesList)   # filter for species of interest
+  
 
-class(bgc_join) # sf, tbl
+
+## join reference layer with rsk BEC, keep all records from ref
+bgc_join <- left_join(ref[, c("BGC", "SS_NoSpace", "Species")], 
+                      bec_rsk[, c("MAP_LABEL", "FEAT_AREA", "ZONE_NAME")], 
+                      by = c("BGC" = "MAP_LABEL"))
+
+## write to file
+write_sf(bgc_join, "../bgc_bec_join.shp")
+
+## anti join for offsite species
+spp_offsite <- bgc_join %>% 
+  anti_join(bec_rsk, by = c("BGC" = "MAP_LABEL"))
+
+## turn back to sf
+spp_offsite <- st_as_sf(spp_offsite)
 
 
-## fix geometry and write to file
-# write_sf(bgc_join, "data/bgc_bec_join.shp")
+## write output
+write_sf(spp_offsite, "../spp_offsite.shp")
+
 
 
 ## anti join for offsite species
 speciesList = c("FD", "LW", "CW", "PY") 
 spp_offsite <-
-  ref %>%
-  select(BGC, SS_NoSpace, Species) %>% 
+  bgc_join %>%
   mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
   filter(Species %in% speciesList) %>%   # filter for species of interest
   anti_join(bec_rsk, ., by=c("MAP_LABEL" = "BGC"))
-
-FD_offsite <-
-  ref %>%
-  select(BGC, SS_NoSpace, Species) %>% 
-  mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
-  filter(Species == "Fd") %>%   # filter for species of interest
-  anti_join(bec_rsk, ., by=c("MAP_LABEL" = "BGC"))
-
-LW_offsite <-
-  ref %>%
-  select(BGC, SS_NoSpace, Species) %>% 
-  mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
-  filter(Species == "Lw") %>%   # filter for species of interest
-  anti_join(bec_rsk, ., by=c("MAP_LABEL" = "BGC"))
-
-CW_offsite <-
-  ref %>%
-  select(BGC, SS_NoSpace, Species) %>% 
-  mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
-  filter(Species == "Cw") %>%   # filter for species of interest
-  anti_join(bec_rsk, ., by=c("MAP_LABEL" = "BGC"))
-
-PY_offsite <-
-  ref %>%
-  select(BGC, SS_NoSpace, Species) %>% 
-  mutate(Species=toupper(Species)) %>% # convert spp to uppercase to match RESULTS
-  filter(Species == "Py") %>%   # filter for species of interest
-  anti_join(bec_rsk, ., by=c("MAP_LABEL" = "BGC"))
-
 
 
 ## stats summary
@@ -81,5 +67,5 @@ spp_sum <- as_tibble(spp_offsite) %>%
             )  # summarize total Fd well spaced
   # filter(BGC!="NANA") # remove NA BGC units
 
-## using st_area to get area by species
+## using st_area to get area by species, but before that, filter for unique
 
